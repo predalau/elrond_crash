7f114a74710bd12b4459e27cd84fffbdc51d15bf
Vladimir Preda <vlpreda1@gmail.com>
2022-08-14 17:49:28 +0200

    Another cors debug

--- app/main.py
+++ app/main.py
@@ -10,16 +10,10 @@ game = Game()
 app = FastAPI()
 
 # TODO make SCHEMAS for all payloads **in progress**
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
