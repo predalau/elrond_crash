8cbc85737737f68c404589a81ddc059e37ec941f
Vladimir Preda <vlpreda1@gmail.com>
2022-11-05 14:14:50 +0100

    Added elrond file

---
+++ app/elrond.py
@@ -0,0 +1,83 @@
+import requests
+from erdpy.accounts import Account, Address
+from erdpy.proxy import ElrondProxy
+from erdpy.transactions import Transaction  # , BunchOfTransactions
+from erdpy import config
+from vars import CHAIN_ID, SC_ADDRESS
+
+sc_gateway = ""
+if CHAIN_ID == "D":
+    sc_gateway = f"https://devnet-gateway.elrond.com"
+elif CHAIN_ID == "T":
+    sc_gateway = f"https://testnet-gateway.elrond.com"  # /address/{SC_ADDRESS}/keys"
+else:
+    sc_gateway = f"https://gateway.elrond.com/"  # address/{SC_ADDRESS}/keys"
+
+ELROND_PROXY = ElrondProxy(sc_gateway)
+ELROND_ACCOUNT = Account(pem_file="wallet.pem")
+ELROND_ACCOUNT.sync_nonce(ELROND_PROXY)
+
+
+def int_to_hex(number: int) -> str:
+    hex_nr = hex(number)[2:]
+    if len(hex_nr) % 2 != 0:
+        hex_nr = "0" + hex_nr
+    return hex_nr
+
+
+def get_all_bets():
+    sc = sc_gateway + "/address/" + SC_ADDRESS + "/keys"
+    bet_funds_hex = "bet_funds.mapped".encode().hex()
+    storage = requests.get(sc).json()["data"]["pairs"]
+    bet_funds = {
+        Address(key.replace(bet_funds_hex, "")).bech32(): int(value, 16) / pow(10, 18)
+        for key, value in storage.items()
+        if bet_funds_hex in key
+    }
+    return bet_funds
+
+
+def get_all_rewards():
+    sc = sc_gateway + "/address/" + SC_ADDRESS + "/keys"
+    reward_funds_hex = "reward_funds.mapped".encode().hex()
+    storage = requests.get(sc).json()["data"]["pairs"]
+    reward_funds = {
+        Address(key.replace(reward_funds_hex, "")).bech32(): int(value, 16) / pow(10, 18)
+        for key, value in storage.items()
+        if reward_funds_hex in key
+    }
+    print(reward_funds)
+
+
+def place_bet(sender: Account, amount):
+    tx = Transaction()
+    tx.nonce = sender.nonce
+    tx.sender = sender.address.bech32()
+    tx.value = str(int(amount * pow(10, 18)))
+    tx.receiver = SC_ADDRESS
+    tx.gasPrice = 1000000000
+    tx.chainID = CHAIN_ID
+    tx.data = "addBetFunds"
+    tx.gasLimit = 6000000
+    tx.version = config.get_tx_version()
+    tx.sign(sender)
+    sent_tx = tx.send_wait_result(ELROND_PROXY, timeout=60)
+    print(sent_tx)
+
+
+def send_rewards(sender: Account, adds: dict):
+    tx = Transaction()
+    tx.nonce = sender.nonce
+    tx.sender = sender.address.bech32()
+    tx.value = str(int(0.00 * pow(10, 18)))
+    tx.receiver = SC_ADDRESS
+    tx.gasPrice = 1000000000
+    tx.chainID = CHAIN_ID
+    tx.data = "multiplyFunds"
+    for address, multiplier in adds.items():
+        tx.data += "@" + Address(address).hex() + "@" + int_to_hex(multiplier)
+    tx.gasLimit = (len(adds.keys()) + 1) * 6000000
+    tx.version = config.get_tx_version()
+    tx.sign(sender)
+    sent_tx = tx.send_wait_result(ELROND_PROXY, timeout=60)
+    print(sent_tx.__dict__)
